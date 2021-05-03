34e27d52fad9a15f2711165ac11bd49539dc4f6e
nickpavini <nickpavini@gmail.com>
2021-05-03 09:57:42 -0700

    UI restructured to now have a desktop and mobile view.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
+import { MobileLayout, DesktopLayout } from './components/Layout.js';
 import { Home } from './components/Home/Home';
 import { SignUp } from './components/SignUp/SignUp';
 import { Login } from './components/Login/Login';
-import { DashBoard } from './components/DashBoard/DashBoard';
+import { MobileDashBoard, DesktopDashBoard } from './components/DashBoard/DashBoard.js';
 import { Account } from './components/Account/Account';
 
 import './custom.css'
 
-export default class App extends Component {
+// hook for returning device mode based on width
+const useViewport = () => {
+    const [width, setWidth] = React.useState(window.innerWidth);
+
+    React.useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleWindowResize);
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
+
+    // these numbers will likely change
+    var mode;
+    if (width < 700)
+        mode = 0; // mobile
+    else
+        mode = 1; // desktop
+
+    return mode;
+}
+
+// layer the app with a hooked component
+const App = () => {
+    var mode = useViewport();
+    return <AppComponent device_mode={mode} />;
+}
+export { App };
+
+class AppComponent extends Component {
     static displayName = App.name;
 
     constructor(props) {
         super(props);
-        this.state = { loading: true, loggedIn: false, uid: null };
+        this.state = {
+            loggedIn: null, loading: true,// loggedIn and loading flag
+            uid: null, account_info: null, safe: null, folders: null, // store important userinfo
+            searchString: null, selectedFolderID: null // what the user is searching for and what they have selected within the safe
+        };
 
         //function bindings
         this.attemptRefresh = this.attemptRefresh.bind(this);
         this.updateUserLoggedIn = this.updateUserLoggedIn.bind(this);
+        this.FetchSafe = this.FetchSafe.bind(this);
+        this.FetchUserFolders = this.FetchUserFolders.bind(this);
+        this.FetchUserInfo = this.FetchUserInfo.bind(this);
+        this.SetSearchString = this.SetSearchString.bind(this);
+        this.SetSelectedFolder = this.SetSelectedFolder.bind(this);
     }
 
     componentDidMount() {
@@ -34,62 +70,107 @@ export default class App extends Component {
             contents = <p>Loading...</p>;
         }
         else {
-            // set path options based on whether or not the user is logged in
-            contents = (
-                <Layout loggedIn={this.state.loggedIn} >
-                    <Route exact path='/' render={() => (
-                        this.state.loggedIn ? (
-                            <Redirect to="/dashboard" />
-                        ) : (
-                                <Home />
-                            )
-                    )} />
-                    <Route path='/login' render={() => (
-                        this.state.loggedIn ? (
-                            <Redirect to="/dashboard" />
-                        ) : (
-                                <Login updateUserLoggedIn={this.updateUserLoggedIn} />
-                            )
-                    )} />
-                    <Route path='/signup' render={() => (
-                        this.state.loggedIn ? (
-                            <Redirect to="/dashboard" />
-                        ) : (
-                                <SignUp />
-                            )
-                    )} />
-                    <Route path='/dashboard' render={() => (
-                        this.state.loggedIn ? (
-                            <DashBoard uid={this.state.uid} />
-                        ) : (
-                                <Redirect to="/login" />
-                            )
-                    )} />
-                    <Route path='/account' render={() => (
-                        this.state.loggedIn ? (
-                            <Account uid={this.state.uid}/>
-                        ) : (
-                                <Redirect to="/login" />
-                            )
-                    )} />
-                </Layout>
-            );
+            // set path options based on whether or not the user is logged in and device mode
+            if (this.props.device_mode == 0)
+                return (
+                    <MobileLayout loggedIn={this.state.loggedIn}>
+                        <Route exact path='/' render={() => (
+                            this.state.loggedIn ? (
+                                <Redirect to="/dashboard" />
+                            ) : (
+                                    <Home />
+                                )
+                        )} />
+                        <Route path='/login' render={() => (
+                            this.state.loggedIn ? (
+                                <Redirect to="/dashboard" />
+                            ) : (
+                                    <Login updateUserLoggedIn={this.updateUserLoggedIn} />
+                                )
+                        )} />
+                        <Route path='/signup' render={() => (
+                            this.state.loggedIn ? (
+                                <Redirect to="/dashboard" />
+                            ) : (
+                                    <SignUp />
+                                )
+                        )} />
+                        <Route path='/dashboard' render={() => (
+                            this.state.loggedIn ? (
+                                <MobileDashBoard uid={this.state.uid} safe={this.state.safe} />
+                            ) : (
+                                    <Redirect to="/login" />
+                                )
+                        )} />
+                        <Route path='/account' render={() => (
+                            this.state.loggedIn ? (
+                                <Account uid={this.state.uid} />
+                            ) : (
+                                    <Redirect to="/login" />
+                                )
+                        )} />
+                    </MobileLayout>
+                );
+            else
+                return (
+                    <DesktopLayout loggedIn={this.state.loggedIn}>
+                        <Route exact path='/' render={() => (
+                            this.state.loggedIn ? (
+                                <Redirect to="/dashboard" />
+                            ) : (
+                                    <Home />
+                                )
+                        )} />
+                        <Route path='/login' render={() => (
+                            this.state.loggedIn ? (
+                                <Redirect to="/dashboard" />
+                            ) : (
+                                    <Login updateUserLoggedIn={this.updateUserLoggedIn} />
+                                )
+                        )} />
+                        <Route path='/signup' render={() => (
+                            this.state.loggedIn ? (
+                                <Redirect to="/dashboard" />
+                            ) : (
+                                    <SignUp />
+                                )
+                        )} />
+                        <Route path='/dashboard' render={() => (
+                            this.state.loggedIn ? (
+                                <DesktopDashBoard uid={this.state.uid} safe={this.state.safe} folders={this.state.folders} selectedFolderID={this.state.selectedFolderID} searchString={this.state.searchString} SetSearchString={this.SetSearchString} SetSelectedFolder={this.SetSelectedFolder} />
+                            ) : (
+                                    <Redirect to="/login" />
+                                )
+                        )} />
+                        <Route path='/account' render={() => (
+                            this.state.loggedIn ? (
+                                <Account uid={this.state.uid} account_info={this.state.account_info} />
+                            ) : (
+                                    <Redirect to="/login" />
+                                )
+                        )} />
+                    </DesktopLayout>
+                );
         }
 
         return (contents);
     }
 
     // call back function for app to set user logged in
-    updateUserLoggedIn(uidStr) {
-        this.setState({ loggedIn: true, uid: uidStr });
+    async updateUserLoggedIn(uidStr) {
+        this.setState({ loading: true, loggedIn: true, uid: uidStr });
+        await this.FetchSafe(); // get accounts
+        await this.FetchUserFolders(); // get users folders
+        await this.FetchUserInfo(); // get user account info
+        this.setState({ loading: false });
     }
 
-    // call back function for app to set user logged out
+    // call back function for app to set user logged out... NOTE: here we will want to clear all user info
     updateUserLoggedOut() {
         this.setState({ loggedIn: false, uid: null });
     }
 
-    // attemp to retrieve a new access token with the existing cookies.. Note that cookies are http only and contain JWT tokens and refresh tokens
+    // attempt to retrieve a new access token with the existing cookies.. Note that cookies are http only and contain JWT tokens and refresh tokens
     async attemptRefresh() {
 
         // http request options
@@ -106,8 +187,74 @@ export default class App extends Component {
             const responseText = await response.text();
             var obj = JSON.parse(responseText);
             this.setState({ loggedIn: true, uid: obj.id });
+            await this.FetchSafe(); // get accounts
+            await this.FetchUserFolders(); // get users folders
+            await this.FetchUserInfo();
+            this.setState({loading: false}); // done loading
         }
+        else
+            this.setState({ loading: false, loggedIn: false});
+    }
 
-        this.setState({ loading: false });
+    //fetch all the users accounts.. may move this to safe, but for now we have it here to easily know whether to display loading or not
+    async FetchSafe() {
+
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include'
+        };
+
+        const reqURI = 'https://eus-safeaccounts-test.azurewebsites.net/users/' + this.state.uid + '/accounts';
+        const response = await fetch(reqURI, requestOptions);
+        if (response.ok) {
+            const responseText = await response.text();
+            this.setState({ safe: JSON.parse(responseText).accounts })
+        }
+    }
+
+    // get all the folders that the users has created
+    async FetchUserFolders() {
+
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include'
+        };
+
+        const reqURI = 'https://eus-safeaccounts-test.azurewebsites.net/users/' + this.state.uid + '/folders';
+        const response = await fetch(reqURI, requestOptions);
+        if (response.ok) {
+            const responseText = await response.text();
+            this.setState({ folders: JSON.parse(responseText).folders })
+        }
+    }
+
+    // get user account info for the settings area.. might move this out of dashboard eventually
+    async FetchUserInfo() {
+
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include'
+        };
+
+        const reqURI = 'https://eus-safeaccounts-test.azurewebsites.net/users/' + this.state.uid;
+
+        const response = await fetch(reqURI, requestOptions);
+        if (response.ok) {
+            const responseText = await response.text();
+            this.setState({ account_info: responseText })
+        }
+    }
+
+    // call back for the sidebar to set search params for the safe
+    SetSearchString(str) {
+        this.setState({ searchString: str });
+    }
+
+    // call back for the Folder component to set selected for the safe
+    SetSelectedFolder(id) {
+        this.setState({ selectedFolderID: id });
     }
 }
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { App } from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
