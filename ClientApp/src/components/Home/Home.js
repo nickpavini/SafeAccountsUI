eea05f9f5f5d87e4fa71fdfb2110165f469df2f5
Chaaban Chaaban <mchaaban1152002@gmail.com>
2021-05-05 14:15:17 -0400

    1-Added meta tags 2- Added title 3- Added a remove on click function and to remove the cookies window when the user clicks on the close icon

--- ClientApp/src/components/Home/Home.js
+++ ClientApp/src/components/Home/Home.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
+import MetaTags from 'react-meta-tags';
+import { ReactTitle } from 'react-meta-tags';
 
 export class Home extends Component {
   static displayName = Home.name;
+  remove (el) {
+    var element = el;
+    element.remove();
+  }
 
   render () {
-    return (
-      <div>
+      return (
+      <div className="wrapper">
+        <MetaTags>
+            <meta charset="utf-8" />
+            {/* Only for debugging the time for refresh will be either changed or removed later*/}
+            <meta http-equiv="refresh" content="1" />
+            <meta name="description" content="" />
+            <meta name="keywords" content="HTML, CSS, JavaScript, React" />
+            {/* Prevents the storage of data in a cache */}
+            <meta http-equiv="cache-control" content="no-cache" /> 
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </MetaTags>
+        <ReactTitle title="Home" />
         <body>
           <p>Never worry about passwords, we got you covered</p>
-          <img src={require("./Assets/securityIcon.png")} alt="SecurityIcon"></img>
+          <img src={require("./Assets/securityIcon.png")} alt="SecurityIcon" />
           <p>Be Safe with</p>
-          <img src="" alt="Logo"></img>
+          <img src="" alt="Logo" />
           <span class="sinup-button">
             <button type="button">Start for free</button>
           </span>
           <div class="cookies-window">
             <pre>[cookies policy go here]</pre>
             <span>
-              <button type="button"><img src={require("./Assets/closeIcon.png")} alt="close-icon"></img></button>
+                <button type="button" onClick="remove(.cookies-window)"><img src={require("./Assets/closeIcon.png")} alt="close-icon"></img></button>
             </span> 
           </div>
         </body>
