cdf5b3bdbc6333582c0b54f18de87ef6c4029e7a
Mohamad Chaaban <59585029+shaybra@users.noreply.github.com>
2021-05-08 21:11:13 -0400

    Home component styling and cookiespolicy component added
    * 1-Added meta tags 2- Added title 3- Added a remove on click function and to remove the cookies window when the user clicks on the close icon
    
    * Added 9 packages that are needed for the meta tags and ReactTitle
    
    * Added some usefull comments where I thought they were needed
    
    * 1- Added some more usefull comments 2- Wrote CSS for both the slogan and the be safe p that's under the safe icon 3-linked the CSS file to the page 4- The start for free button now takes the user to the Sign Up page.
    
    * 1- Fixed a complitation error that was in the remove function comment 2- Used relative path to call on the Sign up page instead of absolute
    
    * Changed some of the units from absolute(px) to relative(em) might revert back in the future if needed
    
    * Changed what I belive to be a wrong link to the css file to a right one, I am going to test it after getting my debugger set.
    
    * 1- Removed closeIcon.png
    2- Setup bootstrap
    3- Styling
    
    * Made a cookiespolicy component, and fixed some problems that arised after a merge.
    
    * Styling home component

--- ClientApp/src/components/Home/Home.js
+++ ClientApp/src/components/Home/Home.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
+import MetaTags from 'react-meta-tags';
+import { ReactTitle } from 'react-meta-tags';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.css';
+import { Link, NavLink } from 'react-router-dom';
+import { CookiesPolicy } from '../CookiesPolicy/CookiesPolicy';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <body>
-          <p>Never worry about passwords, we got you covered</p>
-          <img src={require("./Assets/securityIcon.png")} alt="SecurityIcon"></img>
-          <p>Be Safe with</p>
-          <img src="" alt="Logo"></img>
-          <span class="sinup-button">
-            <button type="button">Start for free</button>
-          </span>
-          <div class="cookies-window">
-            <pre>[cookies policy go here]</pre>
-            <span>
-              <button type="button"><img src={require("./Assets/closeIcon.png")} alt="close-icon"></img></button>
-            </span> 
+    render() {
+      return (
+          <div className="wrapper">
+              <MetaTags>
+                  <meta charset="utf-8" />
+                  <meta name="description" content="" />
+                  <meta name="keywords" content="HTML, CSS, JavaScript, React" />
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              </MetaTags>
+              <ReactTitle title="Home" />
+              <p className="slogan">Never worry about passwords, we got you covered</p>
+              <img src={require("./Assets/securityIcon.png")} alt="SecurityIcon" className="security-icon" />
+              <p className="be-safe">Be Safe with</p>
+              <img src="" alt="Logo" className="logo" />
+              <NavLink to="/signup" className="signup-button-container">
+                      Start for free
+              </NavLink>
           </div>
-        </body>
-      </div>
-    );
+      );
   }
 }
