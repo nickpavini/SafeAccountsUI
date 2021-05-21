bf45b77c2a2804d1fa73523e8b4b03ca8411a1c7
nickpavini <nickpavini@gmail.com>
2021-05-20 21:15:08 -0700

    fix searchbar bug with capital letters

--- ClientApp/src/components/DashBoard/Safe/Safe.js
+++ ClientApp/src/components/DashBoard/Safe/Safe.js
@@ -32,7 +32,7 @@ export class Safe extends Component {
                     this.props.safe.map((value, index) => {
                         // display the account if its folder matches, or the selected folder is null  or empty then we display all
                         if (value.FolderID === this.props.selectedFolderID || this.props.selectedFolderID === null) {
-                            if (value.title.includes(this.props.searchString) || this.props.searchString === null)
+                            if (this.props.searchString === null || value.title.toLowerCase().includes(this.props.searchString.toLowerCase()))
                                 return (
                                     <SafeItem info={value} UpdateSelectedItems={this.UpdateSelectedItems}/>
                                 );
