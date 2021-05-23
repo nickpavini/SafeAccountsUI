03b54ccb01545a6a1d90f90887da3daf0dd90bf6
nickpavini <nickpavini@gmail.com>
2021-05-22 18:29:05 -0700

    add seperate views for page being dashboard, or the editing page for an item, and implement click functionality to redirect to viewing the account.. styling still to be added

--- ClientApp/src/components/DashBoard/Safe/SafeItem/SafeItem.js
+++ ClientApp/src/components/DashBoard/Safe/SafeItem/SafeItem.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import './SafeItem.css';
 
 export class SafeItem extends Component {
@@ -7,29 +8,46 @@ export class SafeItem extends Component {
     constructor(props) {
         super(props);
 
-        this.SetItemSelected = this.SetItemSelected.bind(this);
-    }
+        this.state = {
+            redirect: false,
+            toUrl: null
+        }
 
-    componentDidMount() {
+        // function bindings
+        this.SetItemSelected = this.SetItemSelected.bind(this);
+        this.ViewSafeItem = this.ViewSafeItem.bind(this);
     }
 
     render() {
-        var id = "input_chk_safeitem_" + this.props.info.ID;
-        return (
-            <div class="div_safeitem">
-                <div class="input_chk_safeitem">
-                    <input type="checkbox" defaultChecked={false} onClick={this.SetItemSelected} id={id}></input>
-                </div>
+        // when ready to switch paths
+        if (this.state.redirect)
+            return <Redirect to={this.state.toUrl} />
 
-                <div class="div_safeitem_info">
-                    <a class="safeitem_info" id="a_safeitem_title" href="javascript:;" onClick="">{this.props.info.title}</a><br />
-                    <p class="safeitem_info" id="p_safeitem_login">{this.props.info.login}</p>
+        // if on dashboar, we are rendering a snippet until people open the account or right click to get password
+        if (window.location.pathname === "/dashboard") {
+            var id = "input_chk_safeitem_" + this.props.info.id.toString();
+            return (
+                <div class="div_safeitem" onClick={this.ViewSafeItem}>
+                    <input type="checkbox" defaultChecked={false} onClick={this.SetItemSelected} id={id} class="input_chk_safeitem" ></input>
+                    <span id="span_safeitem_title" onClick={this.ViewSafeItem}>{this.props.info.title}</span><br />
                 </div>
-            </div>
-        );
+            );
+        }
+        else { // in this case we are viewing a single account as a whole page in edit mode
+            return <p>"Hello World!" {this.props.info.id}</p>;
+        }
     }
 
     async SetItemSelected(event) {
         this.props.UpdateSelectedItems(event.target.id.replace("input_chk_safeitem_", ""))
+    }
+
+    // set redirect and go to safeitems path and render in edit mode
+    ViewSafeItem(event) {
+        // if this event got fired as a result of the input being checked, we do nothing
+        if (event.target.id.includes("input_chk"))
+            return;
+
+        this.setState({ redirect: true, toUrl: "/safeitems/" + this.props.info.id.toString() });
     }
 }
