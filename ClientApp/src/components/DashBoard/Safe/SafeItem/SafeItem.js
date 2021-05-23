079536096698680adf45f1c6dd1375f42d9382fd
nickpavini <nickpavini@gmail.com>
2021-05-23 00:06:58 -0700

    add functionality for saving an edit, updating safe and going back to dashboard

--- ClientApp/src/components/DashBoard/Safe/SafeItem/SafeItem.js
+++ ClientApp/src/components/DashBoard/Safe/SafeItem/SafeItem.js
@@ -9,23 +9,34 @@ export class SafeItem extends Component {
         super(props);
 
         this.state = {
-            redirect: false,
-            toUrl: null
+            loading: false, redirect: false, toUrl: null,
+            titleDone: null, loginDone: null, passwordDone: null, // flags for if each individually is good
+            descriptionDone: null, error: false // error flag to display message if needed
         }
 
         // function bindings
         this.SetItemSelected = this.SetItemSelected.bind(this);
         this.ViewSafeItem = this.ViewSafeItem.bind(this);
         this.SaveItemEdit = this.SaveItemEdit.bind(this);
+        this.UpdateDescription = this.UpdateDescription.bind(this);
+        this.UpdateLogin = this.UpdateLogin.bind(this);
+        this.UpdatePassword = this.UpdatePassword.bind(this);
+        this.UpdateDescription = this.UpdateDescription.bind(this);
+        this.CallItemEditAPI = this.CallItemEditAPI.bind(this);
+        this.CheckIfSaveFinished = this.CheckIfSaveFinished.bind(this);
     }
 
     render() {
+        // check if loading
+        if (this.state.loading)
+            return <p>saving...</p>;
+
         // when ready to switch paths
-        if (this.state.redirect)
-            return <Redirect to={this.state.toUrl} />
+        else if (this.state.redirect)
+            return <Redirect to={this.state.toUrl} />;
 
         // if on dashboar, we are rendering a snippet until people open the account or right click to get password
-        if (window.location.pathname === "/dashboard") {
+        else if (window.location.pathname === "/dashboard") {
             var id = "input_chk_safeitem_" + this.props.info.id.toString();
             return (
                 <div class="div_safeitem" onClick={this.ViewSafeItem}>
@@ -74,5 +85,84 @@ export class SafeItem extends Component {
 
     async SaveItemEdit(event) {
         event.preventDefault(); //prevent page refresh
+
+        // get the values in the text fields
+        var title = event.target.text_input_edit_item_title.value;
+        var login = event.target.text_input_edit_item_login.value;
+        var password = event.target.text_input_edit_item_password.value;
+        var description = event.target.text_input_edit_item_description.value;
+
+        // update values as needed
+        this.setState({ loading: true });
+        (title !== this.props.info.title) ? this.UpdateTitle(title) : this.setState({ titleDone: true });
+        (login !== this.props.info.login) ? this.UpdateLogin(login) : this.setState({ loginDone: true });
+        (password !== this.props.info.password) ? this.UpdatePassword(password) : this.setState({ passwordDone: true });
+        (description !== this.props.info.description) ? this.UpdateDescription(description) : this.setState({ descriptionDone: true });
+    }
+
+    async UpdateTitle(value) {
+        var response = await this.CallItemEditAPI("/title", value);
+        if (response.ok) {
+            this.setState({ titleDone: true });
+            this.CheckIfSaveFinished();
+        }
+        else {
+            this.setState({ titleDone: true, error: true, loading: false });
+        }
+    }
+
+    async UpdateLogin(value) {
+        var response = await this.CallItemEditAPI("/login", value);
+        if (response.ok) {
+            this.setState({ loginDone: true });
+            this.CheckIfSaveFinished();
+        }
+        else {
+            this.setState({ loginDone: true, error: true, loading: false });
+        }
+    }
+
+    async UpdatePassword(value) {
+        var response = await this.CallItemEditAPI("/password", value);
+        if (response.ok) {
+            this.setState({ passwordDone: true });
+            this.CheckIfSaveFinished();
+        }
+        else {
+            this.setState({ passwordDone: true, error: true, loading: false });
+        }
+    }
+
+    async UpdateDescription(value) {
+        var response = await this.CallItemEditAPI("/description", value);
+        if (response.ok) {
+            this.setState({ descriptionDone: true });
+            this.CheckIfSaveFinished();
+        }
+        else {
+            this.setState({ descriptionDone: true, error: true, loading: false });
+        }
+    }
+
+    async CallItemEditAPI(endpoint, new_value) {
+        // HTTP request options
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json', 'ApiKey': process.env.REACT_APP_API_KEY },
+            body: "\"" + new_value + "\"",
+            credentials: 'include'
+        };
+
+        //make request and get response
+        var reqUrl = 'https://localhost:44366/users/' + this.props.uid + '/accounts/' + this.props.info.id + endpoint
+        return fetch(reqUrl, requestOptions);
+    }
+
+    // function to check if all the saving has finished
+    async CheckIfSaveFinished() {
+        if (this.state.titleDone && this.state.loginDone && this.state.passwordDone && this.state.descriptionDone) {
+            await this.props.FetchSafe(); //update safe for dashboard.. could probably code this to be much faster than another call
+            this.setState({ loading: false, redirect: true, toUrl: "/dashboard" }); // done loading, lets get back to dashboard
+        }
     }
 }
