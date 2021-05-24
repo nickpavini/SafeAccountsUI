8427c7bd59a1b600d427f0f77a41501001f2a2fd
nickpavini <nickpavini@gmail.com>
2021-05-24 07:43:24 -0700

    add delete item functionality to safe item context menu

--- ClientApp/src/components/DashBoard/Safe/SafeItem/SafeItemContextMenu/SafeItemContextMenu.js
+++ ClientApp/src/components/DashBoard/Safe/SafeItem/SafeItemContextMenu/SafeItemContextMenu.js
@@ -14,6 +14,7 @@ export class SafeItemContextMenu extends Component {
         this.handleClick = this.handleClick.bind(this);
         this.AddItem = this.AddItem.bind(this);
         this.EditSafeItem = this.EditSafeItem.bind(this);
+        this.DeleteSafeItem = this.DeleteSafeItem.bind(this);
     }
 
     componentDidMount() {
@@ -39,6 +40,7 @@ export class SafeItemContextMenu extends Component {
             <div id="menu_safeitem" style={{ top: this.props.top, left: this.props.left }}>
                 <span className="menu_safeitem" id="menu_safeitem_add_item" onClick={this.AddItem}>Add Item</span><br />
                 <span className="menu_safeitem" id="menu_safeitem_edit_item" onClick={this.EditSafeItem}>Edit Item</span><br />
+                <span className="menu_safeitem" id="menu_safeitem_edit_item" onClick={this.DeleteSafeItem}>Delete Item</span><br />
             </div>
         );
     }
@@ -51,5 +53,21 @@ export class SafeItemContextMenu extends Component {
     // set redirect and go to safeitems path and render in edit mode
     EditSafeItem() {
         this.setState({ redirect: true, toUrl: "/safeitems/" + this.props.item.id.toString() });
+    }
+
+    // POST a new item to the safe
+    async DeleteSafeItem() {
+        // HTTP request options
+        const requestOptions = {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json', 'ApiKey': process.env.REACT_APP_API_KEY },
+            credentials: 'include'
+        };
+
+        //make request and get response
+        const response = await fetch('https://localhost:44366/users/' + this.props.uid + '/accounts/' + this.props.item.id.toString(), requestOptions);
+        if (response.ok) {
+            this.props.FetchSafe();
+        }
     }
 }
