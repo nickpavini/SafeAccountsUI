b0c568424752d36476d177db44b2a1bf12737709
nickpavini <nickpavini@gmail.com>
2021-05-23 20:15:36 -0700

    basic context menu for right clicking on an item in the safe. For now first option takes you to the add item area

--- ClientApp/src/components/DashBoard/Safe/SafeItem/SafeItem.js
+++ ClientApp/src/components/DashBoard/Safe/SafeItem/SafeItem.js
@@ -17,15 +17,23 @@ export class SafeItem extends Component {
         this.ViewSafeItem = this.ViewSafeItem.bind(this);
     }
 
+    componentDidMount() {
+        document.getElementById("div_safeitem_" + this.props.info.id.toString()).addEventListener('contextmenu', e => {
+            e.preventDefault();
+            this.props.OpenContextMenu(this.props.info.id, e.pageX + "px", e.pageY + "px");
+        });
+    }
+
     render() {
         // redirect to edit mode upon clicking
         if (this.state.redirect)
             return <Redirect to={this.state.toUrl} />;
 
-        var id = "input_chk_safeitem_" + this.props.info.id.toString();
+        var input_id = "input_chk_safeitem_" + this.props.info.id.toString();
+        var div_id = "div_safeitem_" + this.props.info.id.toString();
         return (
-            <div className="div_safeitem" onClick={this.ViewSafeItem}>
-                <input type="checkbox" defaultChecked={false} onClick={this.SetItemSelected} id={id} className="input_chk_safeitem" ></input>
+            <div id={div_id} className="div_safeitem" onClick={this.ViewSafeItem}>
+                <input type="checkbox" defaultChecked={false} onClick={this.SetItemSelected} id={input_id} className="input_chk_safeitem" ></input>
                 <span id="span_safeitem_title" onClick={this.ViewSafeItem}>{this.props.info.title}</span><br />
             </div>
         );
---
+++ ClientApp/src/components/DashBoard/Safe/SafeItem/SafeItemContextMenu/SafeItemContextMenu.js
@@ -0,0 +1,48 @@
+﻿import React, { Component } from 'react';
+import './SafeItemContextMenu.css';
+import { Redirect } from 'react-router-dom';
+
+export class SafeItemContextMenu extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            redirect: false, toUrl: null
+        }
+
+        // function bindings
+        this.handleClick = this.handleClick.bind(this);
+        this.AddItem = this.AddItem.bind(this);
+    }
+
+    componentDidMount() {
+        document.addEventListener("click", this.handleClick);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("click", this.handleClick);
+    }
+
+    // unrender if we click away
+    async handleClick(e) {
+        this.props.CloseContextMenu();
+    }
+
+    render() {
+        // redirect if needed
+        if (this.state.redirect)
+            return <Redirect to={this.state.toUrl} />
+
+        // menu options
+        return (
+            <div id="menu_safeitem" style={{ top: this.props.top, left: this.props.left }}>
+                <span className="menu_safeitem" id="menu_safeitem_add_item" onClick={this.AddItem}>Add Item</span><br />
+            </div>
+        );
+    }
+
+    // redirect to page for adding an item to the safe
+    AddItem() {
+        this.setState({ redirect: true, toUrl: '/addsafeitem' })
+    }
+}
