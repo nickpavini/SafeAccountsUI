893173e3eeb7400c9b4c25386f5353473b91fc2f
nickpavini <nickpavini@gmail.com>
2021-06-16 09:06:37 -0700

    add logic for having different footers based on logged in or out, as well as mobile or desktop.. For now they will all be the same

--- ClientApp/src/components/Footer/Footer.js
+++ ClientApp/src/components/Footer/Footer.js
@@ -1,8 +1,76 @@
 ﻿import React, { Component } from 'react';
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './Footer.css';
 
 export class Footer extends Component {
+
+    constructor(props) {
+        super(props);
+
+        // function bindings
+        this.footerLoggedOut = this.footerLoggedOut.bind(this);
+        this.footerLoggedOutMobile = this.footerLoggedOutMobile.bind(this);
+        this.footerLoggedOutDesktop = this.footerLoggedOutDesktop.bind(this);
+        this.footerLoggedIn = this.footerLoggedIn.bind(this);
+        this.footerLoggedInMobile = this.footerLoggedInMobile.bind(this);
+        this.footerLoggedInDesktop = this.footerLoggedInDesktop.bind(this);
+
+    }
+
     render() {
-        return <div className="div_footer" style={{ height: this.props.height }} >Inside Footer Component</div>
+        var contents = !this.props.loggedIn
+            ? this.footerLoggedOut()
+            : this.footerLoggedIn();
+
+        return contents;
+    }
+
+    footerLoggedOut() {
+        var contents = this.props.device_mode === localStorage.getItem("DESKTOP_MODE")
+            ? this.footerLoggedOutDesktop()
+            : this.footerLoggedOutMobile();
+
+        return contents;
+    }
+
+    footerLoggedOutDesktop() {
+        return (
+            <div className="div_footer" style={{ height: this.props.height }} >
+                <div id="div_copyright"><FontAwesomeIcon icon={faCopyright} /><span id="span_copyright">2021 Safe Accounts</span></div>
+            </div >
+        );
+    }
+
+    footerLoggedOutMobile() {
+        return (
+            <div className="div_footer" style={{ height: this.props.height }} >
+                <div id="div_copyright"><FontAwesomeIcon icon={faCopyright} /><span id="span_copyright">2021 Safe Accounts</span></div>
+            </div >
+        );
+    }
+
+    footerLoggedIn() {
+        var contents = this.props.device_mode === localStorage.getItem("DESKTOP_MODE")
+            ? this.footerLoggedInDesktop()
+            : this.footerLoggedInMobile();
+
+        return contents;
+    }
+
+    footerLoggedInDesktop() {
+        return (
+            <div className="div_footer" style={{ height: this.props.height }} >
+                <div id="div_copyright"><FontAwesomeIcon icon={faCopyright} /><span id="span_copyright">2021 Safe Accounts</span></div>
+            </div >
+        );
+    }
+
+    footerLoggedInMobile() {
+        return (
+            <div className="div_footer" style={{ height: this.props.height }} >
+                <div id="div_copyright"><FontAwesomeIcon icon={faCopyright} /><span id="span_copyright">2021 Safe Accounts</span></div>
+            </div >
+        );
     }
 }
--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -29,7 +29,7 @@ export class Layout extends Component {
         // set heights for the top bar, bottom bar, and middle content based on mode
         // this prevents the footer from being able to overlap the content in the middle when both use position fixed.
         // I.e. The middle container will always be exactly the height it needs to be
-        var navbarHeight = 55, footerHeight = 70, middleHeight = this.props.windowDimensions.height - navbarHeight - footerHeight;
+        var navbarHeight = 55, footerHeight = 55, middleHeight = this.props.windowDimensions.height - navbarHeight - footerHeight;
 
         // include props from app.js to let navmenu know if it needed to update
         return (
