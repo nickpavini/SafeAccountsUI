697987e013d79a81afb2ca4b4b018bc5786b1558
nickpavini <nickpavini@gmail.com>
2021-06-18 09:38:51 -0700

    style home component

--- ClientApp/src/components/Home/Home.js
+++ ClientApp/src/components/Home/Home.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
 import { NavLink } from 'react-router-dom';
+import { ReactComponent as LogoSvg } from '../../Assets/logo.svg';
 
 export class Home extends Component {
     static displayName = Home.name;
 
     render() {
       return (
-          <div className="wrapper">
-              <p className="slogan">Never worry about passwords, we got you covered</p>
-              <img src={require("./Assets/securityIcon.png")} alt="SecurityIcon" className="security-icon" />
-              <p className="be-safe">Be Safe with</p>
-              <img src="" alt="Logo" className="logo" />
-              <NavLink to="/signup" className="signup-button-container">
-                      Start for free
-              </NavLink>
+          <div className="div_home">
+              <p className="slogan">Never worry about passwords, we got you covered.</p>
+              <LogoSvg id="svg_home_logo" fill="#000"/>
+              <p className="be-safe">Be proactive with Safe Accounts</p>
+              <NavLink to="/signup" className="signup-button-container">Sign Up!</NavLink>
           </div>
       );
   }
