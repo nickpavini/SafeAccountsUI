9c776e3ab8b74b76d3962fcb5f02fb4913b737e2
nickpavini <nickpavini@gmail.com>
2021-07-08 07:57:10 -0700

    add aes encrypt functionality to helper methods

--- ClientApp/src/components/HelperFunctions.js
+++ ClientApp/src/components/HelperFunctions.js
@@ -1,5 +1,22 @@
 ﻿var CryptoJS = require("crypto-js");
 
+// aes encrypt string to hex string
+export function Encrypt(str) {
+    var KEY = window.localStorage.getItem("UserKey").substring(0, 32); //32 bit
+    var IV = "1234567890000000";//16 bits
+    var keyAsBytes = CryptoJS.enc.Utf8.parse(KEY);
+    var iv = CryptoJS.enc.Utf8.parse(IV);
+
+    var srcs = CryptoJS.enc.Utf8.parse(str); // incoming string is not hex string
+    var encrypted = CryptoJS.AES.encrypt(srcs, keyAsBytes, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+
+    return encrypted.ciphertext.toString();
+}
+
 // aes decrypt hex string
 export function Decrypt(str) {
     var KEY = window.localStorage.getItem("UserKey").substring(0, 32); //32 bit
