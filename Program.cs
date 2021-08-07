4b1b463c3384683c44b3b724f28b08ebee9907bc
nickpavini <nickpavini@gmail.com>
2021-08-07 09:28:10 -0700

    new logic for getting env variables at runtime from a .env file.. this will be better used with azure

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -11,7 +12,25 @@ namespace SafeAccountsUI {
     public class Program {
         public static void Main(string[] args)
         {
+            SetEnvVariables();
             CreateHostBuilder(args).Build().Run();
+        }
+
+        private static void SetEnvVariables()
+        {
+            if (!File.Exists(".env"))
+                throw new Exception("No .env file present");
+
+            foreach (var line in File.ReadAllLines(".env")) {
+                var parts = line.Split(
+                    '=',
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length != 2)
+                    continue;
+
+                Environment.SetEnvironmentVariable(parts[0], parts[1]);
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
