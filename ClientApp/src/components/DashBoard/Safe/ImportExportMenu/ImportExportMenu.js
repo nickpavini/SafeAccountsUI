9681c8733204da088b28e6a619c5810344d945a0
nickpavini <nickpavini@gmail.com>
2021-09-05 05:32:39 -0700

    style import export menu a bit

--- ClientApp/src/components/DashBoard/Safe/ImportExportMenu/ImportExportMenu.js
+++ ClientApp/src/components/DashBoard/Safe/ImportExportMenu/ImportExportMenu.js
@@ -1,6 +1,6 @@
 ﻿import React, { Component } from 'react';
-import { AttempRefresh, CloseImportExportMenu } from '../../../HelperFunctions.js'
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { CloseImportExportMenu } from '../../../HelperFunctions.js'
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './ImportExportMenu.css';
 
@@ -13,10 +13,12 @@ export class ImportExportMenu extends Component {
         
         return (
             <div id="import_export_menu">
+                <FontAwesomeIcon id="icon_close_import_menu" icon={faTimes} onClick={() => CloseImportExportMenu()} />
                 <div id="import_export_container">
-                    <span onClick={() => CloseImportExportMenu()}>&times;</span>
-                    <div className="menu_import_export" id="" onClick={null}>Delete Items</div>
-                    <div className="menu_import_export" id="" onClick={null}>Move To {'>'}</div>
+                    <div id="import_export_content">
+                        <div className="menu_import_export" id="div_import_passwords_menu_option" onClick={null}>Import</div>
+                        <div className="menu_import_export" id="div_export_passwords_menu_option" onClick={null}>Export</div>
+                    </div>
                 </div>
             </div>
         );
